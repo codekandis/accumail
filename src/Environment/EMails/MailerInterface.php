7bc2eac7924c21b3367b0f6267487b5d3fe4c039
Christian Ramelow <info@codekandis.net>
2021-10-11 07:54:30 +0200

    Adds a mailer.

---
+++ src/Environment/EMails/MailerInterface.php
@@ -0,0 +1,15 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\AccuMail\Environment\EMails;
+
+/**
+ * Represents the interface of any mailer.
+ * @package codekandis/accumail
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface MailerInterface
+{
+	/**
+	 * Processes the job.
+	 */
+	public function process():void;
+}
